cc5cd11053f51ca4f32d95c636d9bb48e725c54e
Fhenix05 <juanluisparrasosa@gmail.com>
2025-11-22 23:04:17 -0600

    Commit 2: Ordenamiento con BubleSort

--- BubleSort.java
+++ BubleSort.java
@@ -4,6 +4,23 @@ import java.util.List;
 import java.util.Scanner;
 
 public class BubleSort {
+
+       public static void bubbleSort(int[] arr) {
+        int n = arr.length;
+        boolean swapped;
+        for (int i = 0; i < n - 1; i++) {
+            swapped = false;
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+            if (!swapped) break;
+        }
+    }
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         
@@ -23,5 +40,7 @@ public class BubleSort {
             System.out.println("Error al leer el archivo: " + e.getMessage());
             return;
         }
+        int[] arr = numbers.stream().mapToInt(i -> i).toArray();
+        bubbleSort(arr);
     }
 }
